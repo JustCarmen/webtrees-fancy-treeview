c2c034d6294d5a40c68044cdfefe0685ff152c58
JustCarmen <carmen@justcarmen.nl>
2013-11-14 23:35:24 +0100

    Update dompdf custom config file

--- pdf/dompdf-0.6.0/dompdf_config.custom.inc.php
+++ pdf/dompdf-0.6.0/dompdf_config.custom.inc.php
@@ -1,15 +1,15 @@
 <?php 
 //define("DOMPDF_TEMP_DIR", "/tmp");
 //define("DOMPDF_CHROOT", DOMPDF_DIR);
-//define("DOMPDF_UNICODE_ENABLED", false);
+define("DOMPDF_UNICODE_ENABLED", true);
 //define("DOMPDF_PDF_BACKEND", "PDFLib");
 //define("DOMPDF_DEFAULT_MEDIA_TYPE", "print");
 //define("DOMPDF_DEFAULT_PAPER_SIZE", "letter");
 //define("DOMPDF_DEFAULT_FONT", "serif");
 //define("DOMPDF_DPI", 72);
 //define("DOMPDF_ENABLE_PHP", true);
-//define("DOMPDF_ENABLE_REMOTE", true);
-//define("DOMPDF_ENABLE_CSS_FLOAT", true);
+define("DOMPDF_ENABLE_REMOTE", true);
+define("DOMPDF_ENABLE_CSS_FLOAT", true);
 //define("DOMPDF_ENABLE_JAVASCRIPT", false);
 //define("DEBUGPNG", true);
 //define("DEBUGKEEPTEMP", true);
