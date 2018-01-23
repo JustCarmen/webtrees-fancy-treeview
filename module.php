0dd640e340aa827e0fcea08a5116027261f8c704
Rico Sonntag <mail@ricosonntag.de>
2018-01-23 13:08:39 +0100

    Fixed declaration issues.

--- module.php
+++ module.php
@@ -112,7 +112,7 @@ class FancyTreeviewModule extends AbstractModule implements ModuleConfigInterfac
 	}
 
 	/** {@inheritdoc} */
-	public function hasTabContent() {
+	public function hasTabContent(Individual $individual) {
 		if ($this->module()->options('ftv_tab')) {
 			return true;
 		} else {
@@ -121,7 +121,7 @@ class FancyTreeviewModule extends AbstractModule implements ModuleConfigInterfac
 	}
 
 	/** {@inheritdoc} */
-	public function isGrayedOut() {
+	public function isGrayedOut(Individual $individual) {
 		return false;
 	}
 
@@ -336,7 +336,7 @@ class FancyTreeviewModule extends AbstractModule implements ModuleConfigInterfac
 	}
 
 	/** {@inheritdoc} */
-	public function getTabContent() {
+	public function getTabContent(Individual $individual) {
 		global $controller;
 
 		$html = '';
