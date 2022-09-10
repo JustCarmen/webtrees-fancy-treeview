c175871c2434bca6591f2c01daf798a6822de96c
JustCarmen <carmen@justcarmen.nl>
2022-09-10 08:55:35 +0200

    Disable mix-manifest file from laravel entry point

--- webpack.mix.module.js
+++ webpack.mix.module.js
@@ -30,10 +30,6 @@ const dist_dir = 'dist/jc-fancy-treeview';
 //https://github.com/gregnb/filemanager-webpack-plugin
 const FileManagerPlugin = require('filemanager-webpack-plugin');
 
-// Disable mix-manifest.json (https://github.com/laravel-mix/laravel-mix/issues/580#issuecomment-919102692)
-// Prevent the distribution zip file containing an unwanted file
-mix.options({ manifest: false })
-
 if (process.env.NODE_ENV === 'production') {
     mix.styles(config.public_dir + '/css/style.css', config.build_dir + '/style.css')
 } else {
