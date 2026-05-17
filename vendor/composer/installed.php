8afe7d1d13ec133a2413e1ba98a306a138c3355f
JustCarmen <carmen@justcarmen.nl>
2026-05-17 16:55:55 +0200

    Version number of jc-composer-update has been reset

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'justcarmen/jc-fancy-treeview',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '4bb5694ee68ebb5181fdce38a632fda31c5e62a8',
+        'reference' => '116f6ea4351e197eef124ede4d46d953bd17db28',
         'type' => 'webtrees-module',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'justcarmen/jc-common-code' => array(
-            'pretty_version' => 'v1.0.7',
-            'version' => '1.0.7.0',
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
             'reference' => 'b6d0f9f685dfb2ed09dc7419a2f0286f5692c52a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../justcarmen/jc-common-code',
@@ -22,7 +22,7 @@
         'justcarmen/jc-fancy-treeview' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '4bb5694ee68ebb5181fdce38a632fda31c5e62a8',
+            'reference' => '116f6ea4351e197eef124ede4d46d953bd17db28',
             'type' => 'webtrees-module',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
